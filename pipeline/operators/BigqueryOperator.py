3502ce3f2c5d9e7e3e70200538e886c054222c66
Claire Wu <jjudy708618@gmail.com>
2022-03-06 00:20:28 +0800

    remove python3.10 runtime and adjust bigqueryOperator

--- pipeline/operators/BigqueryOperator.py
+++ pipeline/operators/BigqueryOperator.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.cloud.bigquery.schema import SchemaField
+from google.cloud.exceptions import NotFound
 import time
 import datetime
 
@@ -33,17 +34,18 @@ class BigqueryOperator():
         return schema
 
     def get_table(self, table_name, schema_dict: dict, partition_col: dict = None):
-        bq_schema = self._map_dict_to_bq_schema(schema_dict)
-        table = bigquery.Table(table_name, bq_schema)
-        if partition_col:
-            table.time_partitioning = bigquery.TimePartitioning(
-                type_=partition_col['type'],
-                field=partition_col['field']  # name of column to use for partitioning
-            )
+
         try:
+            table = self.client.get_table(table_name)
+        except NotFound:
+            bq_schema = self._map_dict_to_bq_schema(schema_dict)
+            table = bigquery.Table(table_name, bq_schema)
+            if partition_col:
+                table.time_partitioning = bigquery.TimePartitioning(
+                    type_=partition_col['type'],
+                    field=partition_col['field']  # name of column to use for partitioning
+                )
             self.client.create_table(table)
-        except:
-            print(f'{table_name} table exist')
         self.table = table
 
     def load_data2bigquery(self, data: list = None, batch: int = None) -> list:
