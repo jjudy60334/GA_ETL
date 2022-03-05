8897a6a85ed7d7624cf649d2829e54526558832d
Claire Wu <jjudy708618@gmail.com>
2022-03-05 22:48:12 +0800

    add test extractor

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -1,1 +1,24 @@
 from pipeline.extractors import JsonExtractor
+import unittest
+
+
+class TestJsonExtractor(unittest.TestCase):
+
+    def setUp(self):
+        self.file_path = 'tests/test_Json_extractor.json'
+
+    def test_jsonextractor(self):
+        json_extractor = JsonExtractor()
+
+        expect_output = [{
+            "schema": {
+                "event": "str"
+            }
+        },
+            {
+            "schema": {
+                "event": "str"
+            }
+        }]
+        output = json_extractor.extract(self.file_path)
+        self.assertListEqual(expect_output, output)
